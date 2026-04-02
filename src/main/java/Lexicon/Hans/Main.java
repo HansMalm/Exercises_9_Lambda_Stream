aa4d06dd60d6460b6216f67b7f1735d53e5d4f70
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-04-02 10:38:17 +0200

    mod Main.java file
    add sample data

--- src/main/java/Lexicon/Hans/Main.java
+++ src/main/java/Lexicon/Hans/Main.java
@@ -1,8 +1,19 @@
 package Lexicon.Hans;
 
+import java.util.List;
 
 public class Main {
     static void main() {
+
+        //Sample Data
+        List<Person> people = List.of(
+                new Person("Amina", 22, "Stockholm", true),
+                new Person("Erik", 17, "Uppsala", true),
+                new Person("Noah", 34, "Stockholm", false),
+                new Person("Sara", 29, "Gothenburg", true),
+                new Person("Lina", 41, "Malmö", false),
+                new Person("Omar", 19, "Stockholm", true)
+        );
 
     }
 }
